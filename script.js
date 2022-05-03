38ae012cd0a773fe0ec095781429222fbf3555c3
Glitch (weather-mvp-project-mika) <none>
2022-05-03 19:16:14 +0000

    🤟🛏️ Updated with Glitch - Weather Example

---
+++ script.js
@@ -0,0 +1,31 @@
+let cityName;
+let stateCode;
+let date;
+let weatherApi;
+
+$(".submit").click(function () {
+  cityName = $("#cityInput").val();
+  stateCode = $("#stateInput").val();
+
+  console.log(cityName);
+  console.log(stateCode);
+
+  date = new Date().toISOString().split("T")[0];
+  $("#date").html(date);
+
+  weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode}&units=imperial&appid=3bb00f30e525b91a1deb9cbd20254379`;
+
+  fetch(weatherApi)
+    .then(function (response) {
+      return response.json();
+    })
+
+    .then(function (data) {
+      $("#city").html(`${data.name}, ${data.sys.country}`);
+      $("#temp").html(`${data.main.temp}°`);
+      $("#feelsLike").html(`Feel like ${data.main.feels_like}°`);
+      $("#conditions").html(`${data.weather[0].description}`);
+      $("#min").html(`Min Temp: ${data.main.temp_min}°`);
+      $("#max").html(`Max Temp: ${data.main.temp_max}°`);
+    });
+});
